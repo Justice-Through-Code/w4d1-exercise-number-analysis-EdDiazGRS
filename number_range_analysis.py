3aa26a69cc061e109a472e6138ef8395c7772fdc
Ed Diaz <eddiaz.grs@gmail.com>
2023-09-29 17:06:04 -0400

    working code

--- number_range_analysis.py
+++ number_range_analysis.py
@@ -13,9 +13,18 @@ Implement Number Analysis Functions
 # TODO: IN A COMMENT WITHIN EACH DEF, WRITE PSEUDOCODE FOR EACH SOLUTION
 
 def calculate_sum(start, end):
-    """
-    Calculate the sum of numbers within the specified range.
 
+    sum = 0  # to initiate variable
+    for num in range(start, end +1):  # loop from the start until the end of range(must add 1)
+        sum += num         # accumulator 
+    return sum             #returns and stores the calculated sum
+    
+# calculate_sum(4,7)
+
+
+"""
+    Calculate the sum of numbers within the specified range.
+    ehh
     Arguments:
         start (int): The starting number of the range (inclusive).
         end (int): The ending number of the range (inclusive).
@@ -27,21 +36,42 @@ def calculate_sum(start, end):
     # TODO: Return the calculated sum.
 
 def find_smallest_number(start, end):
-    """
-    Find the smallest number within the specified range.
 
+    smallest_number = start  #start is the beginning of the range and to initialize
+    for num in range(start, end + 1):  #looping through the entire range inluding the last int
+            if num < smallest_number:   # condition
+                 smallest_number = num  
+
+    return smallest_number
+
+# find_smallest_number(5,56)
+
+
+
+"""
+   Find the smallest number within the specified range.
+    
     Arguments:
         start (int): The starting number of the range (inclusive).
         end (int): The ending number of the range (inclusive).
 
     Return:
         int: The smallest number within the range.
-    """
+"""
     # TODO: Implement the logic to find the smallest number within the range.
     # TODO: Return the found smallest number.
 
 def find_largest_number(start, end):
-    """
+
+    largest_number = start #start is the beginning of the range and to initialize 
+    for num in range(start, end + 1):  #looping through the entire range inluding the last int
+            if num > largest_number:   # condition
+                 largest_number = num  
+
+    return largest_number
+ 
+# find_largest_number(4,97)
+"""
     Find the largest number within the specified range.
 
     Args:
@@ -55,7 +85,15 @@ def find_largest_number(start, end):
     # TODO: Return the found largest number.
 
 def count_even_numbers(start, end):
-    """
+
+    count = 0   #initialize the accumulator
+    for num in range(start, end + 1):  # loop through the entire range
+         if num %2 == 0:      # check to see if it is an even number
+              count += 1      # increase the count by 1
+    return count          #return the count
+# count_even_numbers(2,20)
+
+"""
     Count the number of even numbers within the specified range.
 
     Args:
@@ -69,7 +107,14 @@ def count_even_numbers(start, end):
     # TODO: Return the count of even numbers.
 
 def count_odd_numbers(start, end):
-    """
+
+    count = 0   #initialize the accumulator
+    for num in range(start, end + 1):  # loop through the entire range
+         if num %2 != 0:      # check to see if it is a odd number
+              count += 1      # increase the count by 1
+    return count         #return the count
+# count_odd_numbers(1,10)
+"""
     Count the number of odd numbers within the specified range.
 
     Args:
